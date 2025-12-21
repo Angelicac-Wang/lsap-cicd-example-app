6b156affcde89a31f84396178bafeb1a94e613e2
Angelicac-Wang <b12705031@ntu.edu.tw>
2025-12-21 13:21:14 +0800

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
   res
     .status(200)
-    .send("<h1>Welcome to the CI/CD Workshop! - happy</h1>");
+    .send("<h1>Welcome to the CI/CD Workshop!</h1>");
 });
 
 app.get("/health", (req, res) => {
